b4a459d2101a45a64f88dda576cd718b14ceacaa
zsofa <hzsofi12@gmail.com>
2022-02-11 14:47:59 +0100

    Initial commit

---
+++ src/main/java/com/example/CarMenu/CarMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarMenu/CarMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarMenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
